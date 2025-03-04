d5596fe0ad9592ab48313a7e5027966903453175
Zaira <zairagonzalezencabo@gmail.com>
2025-03-04 16:41:22 +0100

    Documentacion java creada

--- src/main/java/biblioteca/Biblioteca.java
+++ src/main/java/biblioteca/Biblioteca.java
@@ -1,15 +1,33 @@
 package biblioteca;
 import java.util.ArrayList;
 import java.util.List;
+
+/**
+ * Clase de una Biblioteca con una serie de libors
+ * Permite agregar, eliminar y encontrar libros por titulo y por autor
+ *
+ * @see Libro
+ * @author Zaira Gonzalez Encabo
+ * @version 4.3.2025
+ */
+
 public class Biblioteca {
     // TODO: Documentar estos atributos
+
+    /** Lista de Libros que hay en la biblioteca*/
     private final List<Libro> libros;
+
     // TODO: Documentar este método
+    /** Constructor por defecto de biblioteca SIN libros*/
     public Biblioteca() {
         libros = new ArrayList<>();
     }
     // TODO: Documentar este método.
 // Test: NO HAY QUE TESTEAR ESTE METODO
+    /** Constructor con parámetros
+     *
+     * @param libros Lista de libros con la que se inicializa la biblioteca
+     */
     public Biblioteca(List<Libro> libros) {
         this.libros = libros;
     }
@@ -24,12 +42,23 @@ public class Biblioteca {
         return libros.remove(libro);
     }
     // TODO: Documentar este método
+
+    /**
+     *
+     * @return
+     */
     public List<Libro> getLibros() {
         return libros;
     }
     // TODO: Documentar este método.
 // Test 01: buscar libro existente y comprobar que lo localiza.
 // Test 02: buscar libro NO existente y comprobar que no lo localiza.
+
+    /**
+     * Devuelve un Libro por su titulo si existe o no
+     * @param titulo
+     * @return
+     */
     public Libro encuentraLibroPorTitulo(String titulo) {
         for (Libro libro : libros) {
             if (libro.getTitulo().equals(titulo)) {
@@ -44,6 +73,13 @@ public class Biblioteca {
 // Recuerda: las anotaciones @deprecated y @link a la nueva
 // versión mejorada encuentraLibrosPorAutor(...)
 // En esta ocasión, NO TESTEAREMOS este metodo
+
+    /**
+     * @deprecated Este metodo esta obsoleto
+     * Se recomienda usar {@link #encuentraLibrosPorAutor(String)} en su lugar
+     * @param autor
+     * @return
+     */
     public Libro encuentaLibroPorAutor(String autor) {
         for (Libro libro : libros) {
             if (libro.getAutor().equals(autor)) {
@@ -58,6 +94,14 @@ public class Biblioteca {
 // TODO: Testear este metodo.
 // Test: Comprobar la lista de libros que devuelve para un autor existentes.
 // Test: Comprobar la lista de libros que devuelve para un autor No existente
+
+    /**
+     * Busca los libros de un autor determinado
+     * @since v2.0
+     * Sustituye al metodo {@link #encuentaLibroPorAutor(String)}
+     * @param autor
+     * @return
+     */
     public List<Libro> encuentraLibrosPorAutor(String autor) {
         List<Libro> listaLibros = null;
         for (Libro libro : libros) {
--- src/main/java/biblioteca/Libro.java
+++ src/main/java/biblioteca/Libro.java
@@ -1,11 +1,31 @@
 package biblioteca;
+
+/**
+ * Clase con atributos que representan los libros de la biblioteca
+ *
+ * @see Biblioteca
+ * @author Zaira Gonzalez Encabo
+ * @version 4.3.2025
+ */
+
 public class Libro {
     // TODO: Documentar estos atributos
+    /**
+     * Atributos que forman la clase libro: titulo, autor, anioPublicacion
+     */
     private String titulo;
     private String autor;
     private int anioPublicacion;
     // TODO: Documentar este método
 // TODO: Testear este método
+
+    /**
+     * Constructor con parámetros
+     *
+     * @param titulo titulo de los libros
+     * @param autor autores de los libros
+     * @param anioPublicacion año de la publicacion de los libros
+     */
     public Libro(String titulo, String autor, int anioPublicacion) {
         this.titulo = titulo;
         this.autor = autor;
@@ -18,6 +38,11 @@ public class Libro {
         this.titulo = titulo;
     }
     // TODO: Documentar este método
+
+    /**
+     * Obtiene el autor del libro
+     * @return nombre del autor
+     */
     public String getAutor() {
         return autor;
     }
@@ -25,6 +50,11 @@ public class Libro {
         this.autor = autor;
     }
     // TODO: Documentar este método
+
+    /**
+     * Obtiene el año de la publicacion del libro
+     * @return año de publicación
+     */
     public int getAnioPublicacion() {
         return anioPublicacion;
     }
