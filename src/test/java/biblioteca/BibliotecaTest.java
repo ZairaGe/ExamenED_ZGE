62abaeb12becbf9521e342d4b5f7d5055c0e6c9c
Zaira <zairagonzalezencabo@gmail.com>
2025-03-04 17:06:51 +0100

    Test hechos y testeados

--- src/test/java/biblioteca/BibliotecaTest.java
+++ src/test/java/biblioteca/BibliotecaTest.java
@@ -7,19 +7,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BibliotecaTest {
 
+    Libro libro;
+    Biblioteca biblioteca = new Biblioteca();
+
     @BeforeEach
     void setUp() {
+        libro = new Libro("Orgullo y Prejuicio", "Jane Austen", 1813);
     }
 
     @Test
     void agregarLibro() {
+        assertTrue(biblioteca.agregarLibro(this.libro));
     }
 
     @Test
     void eliminarLibro() {
+        assertFalse(biblioteca.eliminarLibro(this.libro));
     }
 
     @Test
     void encuentraLibrosPorAutor() {
+        biblioteca.agregarLibro(this.libro);
+
+        assertNotNull(biblioteca.getLibros());
+
+        assertEquals(biblioteca.getLibros().size(), 1);
+
+        assertTrue(biblioteca.getLibros().contains(this.libro));
     }
 }
